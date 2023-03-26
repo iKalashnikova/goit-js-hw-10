fb8c1997a14bcab04aa586c9477944bec6779c33
iKalashnikova <ikalashnikova@ukr.net>
2023-03-26 23:45:24 +0200

    finish

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -6,25 +6,23 @@ export default function fetchCountries() {
   countryEl.innerHTML = '';
 
     fetch(`https://restcountries.com/v3.1/name/${inputName}?fields=name,capital,population,flags,languages`)
-        .then(response => {
-    if (!response.ok) {
-      Notiflix.Notify.failure("Oops, there is no country with that name");
-      throw new Error(res.status);
-    }
-            return response.json();
-  })
+    .then(response => {
+        if (!response.ok) {
+        Notiflix.Notify.failure("Oops, there is no country with that name");
+        throw new Error(res.status);}
+        return response.json();
+        })
     .then(country => {
-    if (country.length > 10) {
-      Notiflix.Notify.info(
+        if (country.length > 10) {
+        Notiflix.Notify.info(
         "Too many matches found. Please enter a more specific name."
-      );
-    } else if (country.length >= 2 && country.length <= 10) {
-      renderCountryList(country);
-    }
-    else {
-      renderCountryCard(country);
-     inputEl.value = '';
-  }
+        );
+        } else if (country.length >= 2 && country.length <= 10) {
+        renderCountryList(country);
+        }
+        else {
+        renderCountryCard(country);
+        inputEl.value = '';}
     })
     .catch(error => console.log(error))
 }
